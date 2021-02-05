cc82b7467c36074bb7a79c5d6960aa99be77f628
elena <elen1935@stud.kea.dk>
2021-02-05 01:46:45 +0100

    try random function

--- script.js
+++ script.js
@@ -33,13 +33,17 @@ function getUserSelection() {
     //makeRandomComputerChoice();
 }
 
+let userChoice
+let computerChoice
+let computerRandom
+
 function userClicksRock() {
     //not sure if i need this one since it's already rock from the beginnig
     console.log("userClicksRock");
     document.querySelector("button.rock").removeEventListener("click", userClicksRock);
-    /*document.querySelector("#player1-rock").classList.remove("hidden");
+    document.querySelector("#player1-rock").classList.remove("hidden");
     document.querySelector("#player1-scissors").classList.add("hidden");
-    document.querySelector("#player1-paper").classList.add("hidden");*/
+    document.querySelector("#player1-paper").classList.add("hidden");
     
     //document.getElementsByClassName("rock").style.backgroundImage = "url(hand_rock.png)";
     
@@ -47,6 +51,8 @@ function userClicksRock() {
     document.getElementById("player1").style.backgroundImage = "url('hand_rock.png')";*/
 
     makeRandomComputerChoice()
+    userChoice = rock;
+    determineWinner();
 }
 
 function userClicksPaper() {
@@ -54,7 +60,7 @@ function userClicksPaper() {
     document.querySelector("button.paper").removeEventListener("click", userClicksPaper);
 
     document.querySelector("#player1-rock").classList.add("hidden");
-    //document.querySelector("#player1-scissors").classList.add("hidden");
+    document.querySelector("#player1-scissors").classList.add("hidden");
     document.querySelector("#player1-paper").classList.remove("hidden");
 
     //document.getElementsByClassName("paper").style.backgroundImage = "url(hand_paper.png)";
@@ -63,6 +69,8 @@ function userClicksPaper() {
     document.getElementById("player1").style.backgroundImage = "url('hand_paper.png')";*/
 
     makeRandomComputerChoice()
+    userChoice = paper;
+    determineWinner();
 }
 
 function userClicksScissors() {
@@ -79,10 +87,40 @@ function userClicksScissors() {
     document.getElementById("player1").style.backgroundImage = "url('hand_scissors.png')";*/
 
     makeRandomComputerChoice()
+    userChoice = scissors;
+    determineWinner();
 }
+
 
 function makeRandomComputerChoice() {
     console.log("makeRandomComputerChoice");
+
+    computerRandom = Math.floor(Math.random() * 3) + 1;
+
+    if (computerRandom == 1) {
+        computerChoice = rock;
+        console.log("computer chooses rock");
+        document.querySelector("#player2-rock").classList.remove("hidden");
+        document.querySelector("#player2-scissors").classList.add("hidden");
+        document.querySelector("#player2-paper").classList.add("hidden");
+        
+    }
+
+    if (computerRandom == 2) {
+        computerChoice = paper;
+        console.log("computer chooses paper");
+        document.querySelector("#player2-rock").classList.add("hidden");
+        document.querySelector("#player2-scissors").classList.add("hidden");
+        document.querySelector("#player2-paper").classList.remove("hidden");
+    }
+
+    if (computerRandom == 3) {
+        computerChoice = scissors;
+        console.log("computer chooses scissors");
+        document.querySelector("#player2-rock").classList.add("hidden");
+        document.querySelector("#player2-paper").classList.add("hidden");
+        document.querySelector("#player2-scissors").classList.remove("hidden");
+    }
 
     showAnimations();
 }
@@ -94,6 +132,48 @@ function showAnimations() {
     
     determineWinner();
 }
+
+
+function determineWinner() {
+
+    if (userChoice == rock && computerChoice == rock) {
+        draw()
+    }
+
+    if (userChoice == rock && computerChoice == paper) {
+        loss()
+    }
+
+    if (userChoice == rock && computerChoice == scissors) {
+        win()
+    }
+
+    if (userChoice == paper && computerChoice == rock) {
+        win()
+    }
+
+    if (userChoice == paper && computerChoice == paper) {
+        draw()
+    }
+
+    if (userChoice == paper && computerChoice == scissors) {
+        loss()
+    }
+    if (userChoice == scissors && computerChoice == rock) {
+        loss()
+    }
+
+    if (userChoice == scissors && computerChoice == paper) {
+        win()
+    }
+
+    if (playerChoice == scissors && computerChoice == scissors) {
+        draw()
+    }
+}
+
+
+/*
 
 let winner = "";
 
@@ -118,3 +198,4 @@ function showLose() {
 function showDraw() {
     console.log("Show draw");
 }
+*/
