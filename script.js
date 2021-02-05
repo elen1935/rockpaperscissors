3c35d793843386ac135837a02b2f1cd68a940719
elena <elen1935@stud.kea.dk>
2021-02-05 01:03:38 +0100

    toggle images when clicking buttons

--- script.js
+++ script.js
@@ -2,9 +2,21 @@ window.addEventListener('DOMContentLoaded', start);
 
 function start() {
     console.log("start");
-    //start shake animation
-    document.querySelector("#player1").classList.add("shake");
-    document.querySelector("#player2").classList.add("shake");
+    //hides scissors and paper images
+    document.querySelector("#player1-scissors").classList.add("hidden");
+    document.querySelector("#player1-paper").classList.add("hidden");
+
+    document.querySelector("#player2-scissors").classList.add("hidden");
+    document.querySelector("#player2-paper").classList.add("hidden");
+
+    startShakeAnimation();
+}
+
+function startShakeAnimation() {
+    console.log("startShakeAnimation");
+    //start shake animation - doesn't work
+    document.querySelector("#player1-rock").classList.add("shake");
+    document.querySelector("#player2-rock").classList.add("shake");
 
     getUserSelection();
 }
@@ -24,42 +36,47 @@ function getUserSelection() {
 function userClicksRock() {
     //not sure if i need this one since it's already rock from the beginnig
     console.log("userClicksRock");
-    /*document.querySelector("#player1").classList.remove(".player.paper");
-    document.querySelector("#player1").classList.remove(".player.scissors");
-    document.querySelector("#player1").classList.add(".player.rock");/*/
+    document.querySelector("button.rock").removeEventListener("click", userClicksRock);
+    /*document.querySelector("#player1-rock").classList.remove("hidden");
+    document.querySelector("#player1-scissors").classList.add("hidden");
+    document.querySelector("#player1-paper").classList.add("hidden");*/
     
     //document.getElementsByClassName("rock").style.backgroundImage = "url(hand_rock.png)";
     
-    document.querySelector("#player1").classList.add("hidden");
-    document.getElementById("player1").style.backgroundImage = "url('hand_rock.png')";
+    /*document.querySelector("#player1").classList.add("hidden");
+    document.getElementById("player1").style.backgroundImage = "url('hand_rock.png')";*/
 
     makeRandomComputerChoice()
 }
 
 function userClicksPaper() {
     console.log("userClicksPaper");
-    /*document.querySelector("#player1").classList.remove(".player.rock");
-    document.querySelector("#player1").classList.remove(".player.scissors");
-    document.querySelector("#player1").classList.add(".player.paper");*/
+    document.querySelector("button.paper").removeEventListener("click", userClicksPaper);
+
+    document.querySelector("#player1-rock").classList.add("hidden");
+    //document.querySelector("#player1-scissors").classList.add("hidden");
+    document.querySelector("#player1-paper").classList.remove("hidden");
 
     //document.getElementsByClassName("paper").style.backgroundImage = "url(hand_paper.png)";
 
-    document.querySelector("#player1").classList.add("hidden");
-    document.getElementById("player1").style.backgroundImage = "url('hand_paper.png')";
+    /*document.querySelector("#player1").classList.add("hidden");
+    document.getElementById("player1").style.backgroundImage = "url('hand_paper.png')";*/
 
     makeRandomComputerChoice()
 }
 
 function userClicksScissors() {
     console.log("userClicksScissors");
-    /*document.querySelector("#player1").classList.remove(".player.rock");
-    document.querySelector("#player1").classList.remove(".player.paper");
-    document.querySelector("#player1").classList.add(".player.scissors");*/
+    document.querySelector("button.scissors").removeEventListener("click", userClicksScissors);
+
+    document.querySelector("#player1-rock").classList.add("hidden");
+    document.querySelector("#player1-paper").classList.add("hidden");
+    document.querySelector("#player1-scissors").classList.remove("hidden");
 
     //document.getElementsByClassName("scissors").style.backgroundImage = "url(hand_scissors.png)";
 
-    document.querySelector("#player1").classList.add("hidden");
-    document.getElementById("player1").style.backgroundImage = "url('hand_scissors.png')";
+    /*document.querySelector("#player1").classList.add("hidden");
+    document.getElementById("player1").style.backgroundImage = "url('hand_scissors.png')";*/
 
     makeRandomComputerChoice()
 }
